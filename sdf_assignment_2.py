73be0948702c22fd044ea49537cb171717fb2fb4
Gagan <gkaur21.rrc.ca>
2024-09-21 19:30:29 -0500

    Commit: Updates in the sdf_assignment_2.py file where the section of calculations is completed.

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -21,6 +21,20 @@ print(f"next year: {current_year} type: {type(current_year)}")
 
 #CALCULATIONS
 
+FEDERAL_TAX_RATE_GST = 0.05         # Constant variable
+PROVINCIAL_TAX_RATE_PST = 0.07      # Constant variable 
+price_vehicle = 90000.43
+
+changed_rate_gst = FEDERAL_TAX_RATE_GST * price_vehicle 
+changed_rate_pst = PROVINCIAL_TAX_RATE_PST * price_vehicle 
+
+total_price = price_vehicle + changed_rate_gst + changed_rate_pst
+
+print(f"Pre-tax value: {price_vehicle} Provincial Tax: {changed_rate_pst} Federal Tax: {changed_rate_gst} Total: {total_price}")
+
+print(f"Pre-tax value: ${round(price_vehicle,2):,} Provincial Tax: ${round(changed_rate_pst,2):,} Federal Tax: ${round(changed_rate_gst,2):,} Total: ${round(total_price,2):,}")
+
+
 #LISTS
 
 #TUPLES
