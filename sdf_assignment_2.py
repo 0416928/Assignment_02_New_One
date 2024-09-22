f3661f6112178ab5f118331de42c9d5d08bc26b5
Gagan <gkaur21.rrc.ca>
2024-09-21 19:28:37 -0500

    Commit: Updates in the sdf_assignment_2.py file where the section of Simple Data Types is completed.

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -7,6 +7,18 @@ Date: September,21,2024
 
 #SIMPLE DATA TYPES
 
+name = "Gaganpreet"
+print(f"value: {name} type: {type(name)}")
+
+is_valid_driving_license = False
+print(f"value: {is_valid_driving_license} type: {type(is_valid_driving_license)}")
+
+current_year = 2024
+print(f"this year: {current_year} type: {type(current_year)}")
+
+current_year += 1     # Increase the current year by using operation +=
+print(f"next year: {current_year} type: {type(current_year)}")
+
 #CALCULATIONS
 
 #LISTS
